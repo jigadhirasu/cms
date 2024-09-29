c21f055ac893d01a8020b106f376f4539ee7cdf7
Emma <emma@tokenize.exchange>
2024-09-29 18:30:54 +0800

    fix:update emotion

--- src/app/pages/voice/pages/emotion/emotion.component.ts
+++ src/app/pages/voice/pages/emotion/emotion.component.ts
@@ -1,16 +1,10 @@
 import { Component } from '@angular/core';
-import { SearchComponent } from "../../../layout/search/search.component";
 
 @Component({
   selector: 'app-emotion',
   standalone: true,
-  imports: [
-    SearchComponent,
-    SearchComponent,
-  ],
+  imports: [],
   templateUrl: './emotion.component.html',
-  styleUrl: './emotion.component.scss'
+  styleUrl: './emotion.component.scss',
 })
-export class EmotionComponent {
-
-}
+export class EmotionComponent {}
